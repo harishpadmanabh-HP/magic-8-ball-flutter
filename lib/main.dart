eedde5ef3ddad9324e86cd7efa7e0b71b78d5146
harishpadmanabh-HP <harishpadmanabh.sics@gmail.com>
2020-03-31 22:43:46 +0530

    decision

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('8 ball Decision'),
+            backgroundColor: Colors.blue.shade900,
+          ),
+          body: HomePage(),
+        ),
       ),
     );
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          //on click
+          changeBall();
+        },
+      ),
+    );
+  }
+
+  void changeBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+}
